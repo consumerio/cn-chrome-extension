12d0f8e0be5b4b41073a47852b93bc71d71e4ab9
Michael Komitee <mkomitee@gmail.com>
2012-02-23 23:09:29 -0500

    Import

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+(function(){var cn_script_tag=document.createElement('SCRIPT');cn_script_tag.type='text/javascript';cn_script_tag.src='https://s3.amazonaws.com/consumernotebook/js/bookmarklet.js?x='+(Math.random());document.getElementsByTagName('head')[0].appendChild(cn_script_tag);})();
