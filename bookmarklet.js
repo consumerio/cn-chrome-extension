1ce30a5cd9785e6e77aa706ecd0f4a0c32b5df43
Michael Komitee <mkomitee@gmail.com>
2012-02-25 00:52:56 -0500

    Working with a more current Chrome release

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,4 @@
-(function(){var cn_script_tag=document.createElement('SCRIPT');cn_script_tag.type='text/javascript';cn_script_tag.src='https://s3.amazonaws.com/consumernotebook/js/bookmarklet.js?x='+(Math.random());document.getElementsByTagName('head')[0].appendChild(cn_script_tag);})();
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript(
+      null, {code: "(function(){var cn_script_tag=document.createElement('SCRIPT');cn_script_tag.type='text/javascript';cn_script_tag.src='https://s3.amazonaws.com/consumernotebook/js/bookmarklet.js?x='+(Math.random());document.getElementsByTagName('head')[0].appendChild(cn_script_tag);})();"});
+});
